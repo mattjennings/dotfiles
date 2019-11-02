22b426ac1947cb5c73b627fa8836361e12901a56
Matt Jennings <mattjennings44@gmail.com>
2019-11-02 17:04:25 -0600

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -41,6 +41,7 @@ module.exports = {
     "@typescript-eslint/no-var-requires": "off",
       
     /* React */
+     "react/prop-types": "off",
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn",
   },
