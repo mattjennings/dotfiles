12fbc55f40e5e0521e07f6248b0b2a5b8f209f51
Matt Jennings <mattjennings44@gmail.com>
2020-01-02 14:37:22 -0700

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,14 @@
  * Typescript & React
  * 
  * packages:
-    "@typescript-eslint/eslint-plugin": "^2.6.0",
-    "@typescript-eslint/parser": "^2.6.0",
-    "eslint": "^6.6.0",
-    "eslint-config-prettier": "^6.5.0",
-    "eslint-plugin-react": "^7.16.0",
-    "eslint-plugin-react-hooks": "^2.2.0",
-    "prettier": "^1.18.2"
+    "@typescript-eslint/eslint-plugin": "^2.14.0",
+    "@typescript-eslint/parser": "^2.14.0",
+    "eslint": "^6.8.0",
+    "eslint-config-prettier": "^6.9.0",
+    "eslint-plugin-react": "^7.17.0",
+    "eslint-plugin-react-hooks": "^2.3.0",
+    "prettier": "^1.18.2",
+    "typescript": "^3.7.4"
  */
 module.exports = {
   parser: "@typescript-eslint/parser",
